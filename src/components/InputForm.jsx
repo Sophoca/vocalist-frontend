074f628016698817a488a9c4a227e32c292c354a
Hong <tjdqls1668@ajou.ac.kr>
2021-10-07 05:11:00 +0900

    Add curation type list component

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,29 +1,29 @@
 import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 import Autocomplete from '@mui/material/Autocomplete';
-import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
-import CheckBoxIcon from '@mui/icons-material/CheckBox';
-
-const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
-const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
 export default function InputForm({ clist, musicLists }) {
   const [inputs, setInputs] = useState({
     title: '',
     description: '',
     ctype: '',
-    search: ''
+    selectMusic: ''
   });
 
-  const { title, description, ctype, search } = inputs;
+  const { title, description, ctype, selectMusic } = inputs;
 
   const onChange = e => {
-    const { value, name, ctype, search } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+    const { value, id } = e.target; // 우선 e.target 에서 name 과 value 를 추출
     setInputs({
       ...inputs, // 기존의 input 객체를 복사한 뒤
-      [name]: value // name 키를 가진 값을 value 로 설정
+      [id]: value // name 키를 가진 값을 value 로 설정
     });
+    console.log('test', e.target);
   };
 
   const onReset = () => {
@@ -32,39 +32,59 @@ export default function InputForm({ clist, musicLists }) {
     });
   };
 
+  console.log('test', inputs);
+
   return (
     <Box
       component="form"
       sx={{
-        '& .MuiTextField-root': { m: 1, width: '50ch' },
-        border: '1px dashed grey'
+        '& .MuiTextField-root': { width: '400px' },
+        '& .MuiSelect-root': { width: '400px' },
+        display: 'flex',
+        gap: '20px',
+        flexWrap: 'wrap',
+        border: '1px dashed grey',
+        borderRadius: 5,
+        padding: '20px'
       }}
     >
-      <div>
-        <p>curation title</p>
-        <input name="title" placeholder="title"></input>
-        <p>curation description</p>
-        <textarea name="description" placeholder="description"></textarea>
-        <p>search music</p>
-        <input name="search" placeholder="search"></input>
-      </div>
-      <div>
-        <p>music list</p>
-        <Autocomplete
-          multiple
-          id="tags-standard"
-          disableCloseOnSelect
-          autoComplete
-          onChange={(event, newValue) => console.log('test', newValue)}
-          options={musicLists}
-          getOptionLabel={option => `${option.title} - ${option.artist}`}
-          renderInput={params => (
-            <TextField {...params} variant="standard" label="Music List" placeholder="search" />
-          )}
-          sx={{ width: 200 }}
-        />
-        {/* <ComboBox list={ctype} name="curation type" /> */}
-      </div>
+      <TextField id="title" label="Title" variant="standard" onChange={onChange} />
+      <TextField
+        id="description"
+        label="Description"
+        variant="standard"
+        multiline
+        maxRows={5}
+        onChange={onChange}
+      />
+      {/* <p>search music</p>
+      <input name="search" placeholder="search"></input> */}
+      <FormControl>
+        <InputLabel>Type</InputLabel>
+        <Select
+          value={ctype}
+          label="Type"
+          onChange={e => setInputs({ ...inputs, ctype: e.target.value })}
+        >
+          <MenuItem value={0}>None</MenuItem>
+          {clist.map(el => (
+            <MenuItem value={el.id}>{el.title}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <Autocomplete
+        multiple
+        id="musicList"
+        disableCloseOnSelect
+        autoComplete
+        onChange={(event, newValue) => setInputs({ ...inputs, selectMusic: newValue })}
+        options={musicLists}
+        getOptionLabel={option => `${option.title} - ${option.artist}`}
+        renderInput={params => (
+          <TextField {...params} variant="standard" label="Music List" placeholder="search" />
+        )}
+      />
+      {/* <ComboBox list={ctype} name="curation type" /> */}
     </Box>
   );
 }
