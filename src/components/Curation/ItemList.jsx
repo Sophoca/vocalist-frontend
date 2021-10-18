746ad4b77bd195432117efe8f8800ae49a9c937a
Hong <tjdqls1668@ajou.ac.kr>
2021-10-18 15:42:51 +0900

    Fix curation item list

--- src/components/Curation/ItemList.jsx
+++ src/components/Curation/ItemList.jsx
@@ -16,7 +16,7 @@ export default function ItemList({ curation_id, musicLists }) {
       const response = await CurationApi.getCuration(id);
       return response.data;
     }
-    return 'init';
+    return null;
   }
   const [state, refetch] = useAsync(() => fetchData(curation_id), [curation_id]);
   const { loading, data, error } = state;
@@ -35,7 +35,12 @@ export default function ItemList({ curation_id, musicLists }) {
 
   if (loading) return <div style={{ margin: 10, width: 33 + '%' }}>Loading...</div>;
   if (error) return <div>{error}</div>;
-  if (!data) return <div>no data</div>;
+  if (!data)
+    return (
+      <div className="container">
+        <h2>Select Curation</h2>
+      </div>
+    );
 
   const extractID = data.body ? data.body.map(el => el.id) : [];
   const restMusicList = musicLists.filter(el => !extractID.includes(el.id));
@@ -43,55 +48,47 @@ export default function ItemList({ curation_id, musicLists }) {
 
   return (
     <div className="container">
-      <h2>{curation_id ? `Modify #${curation_id} Curation` : 'Select Curation'}</h2>
+      <h2>{`Modify #${curation_id} Curation`}</h2>
       <Box className="box" component="div">
-        {data === 'init' ? null : (
-          <>
-            <Box className="box" component="div">
-              <h3 style={{ margin: 0 }}>Add Items</h3>
-              <AutoComplete
-                musicLists={restMusicList}
-                inputs={inputs}
-                errors
-                setInputs={setInputs}
-              ></AutoComplete>
-            </Box>
-            <Box className="box" component="div">
-              <h3 style={{ margin: 0 }}>Delete Items</h3>
-              <List>
-                {data.body.map(el => (
-                  <ListItem
-                    key={el.id}
-                    sx={{ paddingTop: 0, paddingBottom: 0 }}
-                    secondaryAction={
-                      <IconButton
-                        edge="end"
-                        aria-label="delete"
-                        onClick={() => handleDelete(el.id)}
-                      >
-                        <DeleteIcon />
-                      </IconButton>
-                    }
-                  >
-                    <ListItemText
-                      primary={el.title}
-                      secondary={el.artist}
-                      secondaryTypographyProps={
-                        inputs.delList.includes(el.id) ? { color: 'red' } : null
-                      }
-                      sx={
-                        inputs.delList.includes(el.id)
-                          ? { secondary: 'red', color: 'red', textDecoration: 'line-through' }
-                          : null
-                      }
-                    />
-                  </ListItem>
-                ))}
-              </List>
-            </Box>
-            <Button variant="contained">Modify Curation</Button>
-          </>
-        )}
+        <Box className="box" component="div">
+          <h3 style={{ margin: 0 }}>Add Items</h3>
+          <AutoComplete
+            musicLists={restMusicList}
+            inputs={inputs}
+            errors
+            setInputs={setInputs}
+          ></AutoComplete>
+        </Box>
+        <Box className="box" component="div">
+          <h3 style={{ margin: 0 }}>Delete Items</h3>
+          <List>
+            {data.body.map(el => (
+              <ListItem
+                key={el.id}
+                sx={{ paddingTop: 0, paddingBottom: 0 }}
+                secondaryAction={
+                  <IconButton edge="end" aria-label="delete" onClick={() => handleDelete(el.id)}>
+                    <DeleteIcon />
+                  </IconButton>
+                }
+              >
+                <ListItemText
+                  primary={el.title}
+                  secondary={el.artist}
+                  secondaryTypographyProps={
+                    inputs.delList.includes(el.id) ? { color: 'red' } : null
+                  }
+                  sx={
+                    inputs.delList.includes(el.id)
+                      ? { secondary: 'red', color: 'red', textDecoration: 'line-through' }
+                      : null
+                  }
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Button variant="contained">Modify Curation</Button>
       </Box>
     </div>
   );
