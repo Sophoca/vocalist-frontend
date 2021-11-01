95c367e9d1eea179261c9a1e2b26f696be893990
Hong <tjdqls1668@ajou.ac.kr>
2021-11-02 03:51:45 +0900

    Add bug card component

--- src/components/BugReport/BugCard.jsx
+++ src/components/BugReport/BugCard.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { Card, CardContent, Typography, CardActionArea, Divider } from '@mui/material';
 
 export default function BugCard({ info }) {
-  return null;
+  return (
+    <Card>
+      {/* <CardMedia component="img" height="140" image={image} alt="curation cover img" /> */}
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {info.title}
+        </Typography>
+        <Typography variant="p" color="text.secondary">
+          {info.content}
+        </Typography>
+        <Typography sx={{ fontSize: 12 }} color="text.secondary" mt={2}>
+          작성자: {info.name}
+        </Typography>
+        <Typography sx={{ fontSize: 12 }} color="text.secondary">
+          email: {info.email}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
