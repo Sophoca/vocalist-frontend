ac3f90c2208c0c0f143f49ddc94123d4292f74d7
Hong <tjdqls1668@ajou.ac.kr>
2021-11-13 23:30:38 +0900

    Add chart component with temp data

--- src/pages/Chart.js
+++ src/pages/Chart.js
@@ -1,9 +1,69 @@
 import React from 'react';
 
-const tempData = [];
+import MusicList from 'components/Playlist/MusicList';
 
 const Chart = () => {
-  return <div>chart</div>;
+  return (
+    <>
+      <h2>Chart</h2>
+      <MusicList list={tempData} />
+    </>
+  );
 };
 
 export default Chart;
+
+const tempData = [
+  {
+    id: 7,
+    itunes_id: 3108,
+    title: '철부지',
+    artist: '딕펑스',
+    genre: '팝 > 발라드, 팝 > 팝 락',
+    time: 237,
+    year: 2013,
+    cluster: null,
+    youtube: null,
+    number: null,
+    islike: 0
+  },
+  {
+    id: 9,
+    itunes_id: 3112,
+    title: 'Love and Hate',
+    artist: '보아',
+    genre: 'Ballad',
+    time: 244,
+    year: 2015,
+    cluster: null,
+    youtube: null,
+    number: null,
+    islike: 0
+  },
+  {
+    id: 10,
+    itunes_id: 3114,
+    title: 'Smash',
+    artist: '보아',
+    genre: 'K-Pop',
+    time: 153,
+    year: 2015,
+    cluster: null,
+    youtube: null,
+    number: null,
+    islike: 0
+  },
+  {
+    id: 1,
+    itunes_id: 3096,
+    title: '한번만 더',
+    artist: '나얼',
+    genre: 'R&B',
+    time: 336,
+    year: 2005,
+    cluster: null,
+    youtube: null,
+    number: null,
+    islike: 0
+  }
+];
