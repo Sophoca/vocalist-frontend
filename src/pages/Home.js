37be17b9faff8b26f33708d262d199bdc382fb84
Hong <tjdqls1668@ajou.ac.kr>
2021-11-18 20:20:55 +0900

    Revert "Rollback"
    This reverts commit bafe455f879c2d93fae64029f07b50c85738f2e2.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,7 +20,7 @@ SwiperCore.use([Mousewheel, Pagination]);
 const Container = styled.div`
   width: 100%;
   height: 100vh;
-  // height: calc(var(--vh, 1vh) * 100);
+  height: calc(var(--vh, 1vh) * 100);
   background: white;
 `;
 
